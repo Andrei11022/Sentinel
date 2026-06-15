b375753d81abad5ebdd8a91a3af0bb81244b4ad6
Andrei11022 <127621379+Andrei11022@users.noreply.github.com>
2026-06-15 06:04:46 +0300

    Add files via upload

--- api/forecast.js
+++ api/forecast.js
@@ -50,7 +50,7 @@ function calculateEscalationProbability(countryCode, recentEvents) {
   };
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   const { type, articles, country } = req.method === 'POST'
